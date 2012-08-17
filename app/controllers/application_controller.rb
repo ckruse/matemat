41a1ed1fda6e500b59a07fd428eda4997dd93c84
Christian Kruse <cjk@defunced.de>
2012-08-17 17:35:59 +0200

    fix: login and authorization are two different things

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,12 @@ class ApplicationController < ActionController::Base
 
   def authorize_admin
     usr = current_user
-    redirect_to(root_url, alert: I18n.t('authorize_first')) if usr.nil? or !usr.admin
+    redirect_to(root_url, alert: I18n.t('login_first') if usr.blank?
+    redirect_to(root_url, alert: I18n.t('authorize_first')) if !usr.blank? and !usr.admin
+  end
+
+  protected
+  def not_authenticated
+    redirect_to root_path, alert: I18n.t('login_first')
   end
 end
